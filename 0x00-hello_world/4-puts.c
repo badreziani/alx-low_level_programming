4242ba7468028b82487c794cb251947689355348
badre Ziani <badreziani@gmail.com>
2023-03-16 13:09:08 +0000

    Fixing betty's warnings and errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 /**
- * This is the entry point of our programm
+ * main - Entry point
+ *
+ * Description: This programm prints some the text on the screen
+ *
+ * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
