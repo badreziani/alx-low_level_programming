3c4e380f908bcf0f74bdb534534802dceeef402f
Badre ZIANI <badreziani@gmail.com>
2023-03-17 15:44:50 -0700

    Change the decription of the 8th task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int l = 97;
+
+	while (l <= 122)
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
