d45c17813146b1f100734c48c41c3abda6844e9c
Badre ZIANI <badreziani@gmail.com>
2023-03-21 17:40:27 -0700

    10th task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add 2 numbers
+ * Description: Adds 2 numbers and returns the result
+ * @a: &st number
+ * @b: second number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
