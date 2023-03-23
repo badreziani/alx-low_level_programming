0873d12c3098509f9d8de9440f48d7dd1bd72b68
Badre ZIANI <badreziani@gmail.com>
2023-03-23 10:21:30 -0700

    '0. isupper' task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: character
+ * Return: 1 if c is upper, 0 if not
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else if (c <= 97 && c >= 122)
+		return (0);
+}
