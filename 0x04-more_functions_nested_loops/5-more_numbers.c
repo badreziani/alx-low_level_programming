07c92ed94f8266da920402072bc1cad5023582e9
Badre ZIANI <badreziani@gmail.com>
2023-03-23 12:29:44 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,3 +5,17 @@
  * @b: 2nd integer
  * Return: a*b
  */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
