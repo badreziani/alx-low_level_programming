51dbf84552b3f5746db8ae6754c24b1fa9f39e08
Badre ZIANI <badreziani@gmail.com>
2023-03-23 11:28:30 -0700

    '3. The numbers speak for themselves' task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,16 @@
 #include "main.h"
 /**
- * mul - multiplies two integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: a*b
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ * Return: void
  */
+
+void print_numbers(void)
+{
+	int i = 48;
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
