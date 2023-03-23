8f5a9bb115a2690d953d2e786b7204e26a62593e
Badre ZIANI <badreziani@gmail.com>
2023-03-23 16:11:04 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
+
 /**
- * mul - multiplies two integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: a*b
+ * more_numbers -  checks for checks for a digit (0 through 9)
+ * Return: 0
  */
 void more_numbers(void)
 {
