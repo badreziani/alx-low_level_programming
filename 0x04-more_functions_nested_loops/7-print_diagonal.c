fcef4ebf8f2827141d728d6c6a9440832dadd482
Badre ZIANI <badreziani@gmail.com>
2023-03-23 16:21:06 -0700

    '7. I feel like I am diagonally parked in a parallel universe' task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,32 @@
 #include "main.h"
+
 /**
- * mul - multiplies two integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: a*b
+ * print_diagonal -  checks for checks for a digit (0 through 9)
+ * @n: integer
+ * Return: nothing
  */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if (i == j)
+				{
+					_putchar(92);
+					break;
+				}
+				_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
