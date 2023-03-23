21f4241bc52c7535b8eb1f60aafc1b042abacb9a
Badre ZIANI <badreziani@gmail.com>
2023-03-23 04:37:06 -0700

    Fix betty errors

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - Test the positive_or_negative function
- * Retrun: 0
+ * Return: 0
  */
 
 int main(void)
@@ -10,6 +10,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	
-	return (0);	
+	return (0);
 }
