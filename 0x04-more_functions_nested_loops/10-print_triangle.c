9ad18a613c06bc2e909620fc7c636cf6651f5a2d
Badre ZIANI <badreziani@gmail.com>
2023-03-23 16:47:22 -0700

    '10. Triangles' task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,34 @@
 #include "main.h"
+
 /**
- * mul - multiplies two integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: a*b
+ * print_triangle -  checks for checks for a digit (0 through 9)
+ * @size: Integer
+ * Return: nothing
  */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= size - i)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n')
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
