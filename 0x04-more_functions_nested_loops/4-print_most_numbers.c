6f32a6186d203707c1f648701bc7a8164f00f4d0
Badre ZIANI <badreziani@gmail.com>
2023-03-23 12:08:47 -0700

    '4. I believe in numbers and signs' task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,21 @@
 #include "main.h"
+
 /**
- * mul - multiplies two integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: a*b
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ * Return: void
  */
+
+void print_numbers(void)
+{
+	int i = 48;
+	
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+
+		_putchar(i);
+	}
+	_putchar('\n');
+}
