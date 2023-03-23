2e77c72c24f179fcc97b11ed7de2947179ba38df
Badre ZIANI <badreziani@gmail.com>
2023-03-23 16:25:50 -0700

    '8. You are so much sunshine in every square inch' task

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,27 @@
 #include "main.h"
+
 /**
- * mul - multiplies two integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: a*b
+ * print_square -  checks for checks for a digit (0 through 9)
+ * @size: integer
+ * Return: nothing
  */
+void print_square(int size)
+{
+	int i, j;
+	
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}	
+}
