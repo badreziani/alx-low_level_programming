792bfb6430edce62b15154b926379f9603074a04
Badre ZIANI <badreziani@gmail.com>
2023-03-23 16:15:44 -0700

    '6. The shortest distance between two points is a straight line' task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,20 @@
 #include "main.h"
+
 /**
- * mul - multiplies two integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: a*b
+ * print_line -  checks for checks for a digit (0 through 9)
+ * @n: 1st integer
+ * Return: nothing
  */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
