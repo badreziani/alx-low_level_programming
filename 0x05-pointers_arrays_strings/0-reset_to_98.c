8bf21684df4358a65682c68fbdf0e6677595153b
Badre ZIANI <badreziani@gmail.com>
2023-03-27 05:07:45 -0700

    Fix betty warning

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter and
  * updates the value it points to to 98
- *
+ * @n: a pointer to an interger
  * Returns: nothing
  */
 void reset_to_98(int *n)
