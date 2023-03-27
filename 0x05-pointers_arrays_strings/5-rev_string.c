f69e839287a076184bab7526b06fdcee426ee839
Badre ZIANI <badreziani@gmail.com>
2023-03-27 07:31:42 -0700

    Task 5, reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - reverses a given string
+ * @s: pointer to the string to reverse
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int l; /* to store lenght of the string */
+	int i;
+	char tmp;
+
+	for (i = 0; i <= l / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = tmp;
+	}
+}
