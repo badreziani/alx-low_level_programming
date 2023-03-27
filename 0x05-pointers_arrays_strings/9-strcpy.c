b512b79a14ea312164800d74f38c80366642f397
Badre ZIANI <badreziani@gmail.com>
2023-03-27 11:28:21 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,13 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	i = 0;
-	while (src[i] != '\0')
+
+	while (1)
 	{
 		dest[i] = src[i];
 		i++;
+		if (src[i] == '\0')
+			break;
 	}
 
 	return (dest);
