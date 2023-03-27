a10bfefd2b13f8ed435f2f3a7673d1329e917d2a
Badre ZIANI <badreziani@gmail.com>
2023-03-27 11:06:27 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line
+ * @a: pointer to an int variable
+ * @n: integer - number of elements
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i] + '0');
+		if (i < n - 1)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
