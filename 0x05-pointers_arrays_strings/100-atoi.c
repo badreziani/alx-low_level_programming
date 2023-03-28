3f33d604bf225b6275384e22b1c2b35447bfaed7
Badre ZIANI <badreziani@gmail.com>
2023-03-27 17:12:44 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,30 @@
  */
 int _atoi(char *s)
 {
-	int i, j;
-	int r; /* The result to return */
+	int i;
+	int r; /* the result to return */
+	int si; /* positive or negative number*/
+	int f; /* a flag to check whether we loop through an integer */
 
+	i = 0;
 	r = 0;
-	j = 0;
+	f = 0;
+	si = -1;
 
-	if (_strlen(s) > 0)
+	while (s[i] != '\0')
 	{
-		if (s[j] == '-')
-			j = 1;
+		if (s[i] == '-')
+			si *= -1;
 
-		for (i = j; s[i] != '\0'; i++)
+		if (s[i] >= 48 && s[i] <= 57)
 		{
-			r = r * 10 + (s[i] - 48);
+			r *= 10 + (s[i] - 48);
+			f = 1;
 		}
-
-		if (j == 1)
-			r *= -1;
+		else if (f == 1)
+			break;
+		i++;
 	}
-
+	r *= si;
 	return (r);
 }
