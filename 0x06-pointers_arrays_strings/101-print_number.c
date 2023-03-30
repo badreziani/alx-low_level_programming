9657db6364e5383fd77da2d216bb072db6175af5
Badre ZIANI <badreziani@gmail.com>
2023-03-30 06:59:31 -0700

    tasl 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,35 +7,30 @@
  */
 void print_number(int n)
 {
-	int tmp, p;
-	int no_d = 0;
+	unsigned int tmp, p, no_d;
 
-	if (n != 0)
+	tmp = n;
+	p = 1;
+	no_d = 0;
+	if (n < 0)
 	{
-		tmp = n;
-		p = 1;
-		if (n < 0)
-		{
-			tmp *= -1;
-			n *= -1;
-			_putchar('-');
-		}
-		while (tmp > 0)
-		{
-			no_d++;
-			tmp /= 10;
-		}
-		while (no_d > 1)
-		{
-			no_d--;
-			p *= 10;
-		}
-		while (p >= 1)
-		{
-			_putchar((n / p) % 10 + '0');
-			p /= 10;
-		}
+		tmp *= -1;
+		n *= -1;
+		_putchar('-');
 	}
-	else
-		_putchar(0 + '0');
+	while (tmp > 0)
+	{
+		no_d++;
+		tmp /= 10;
+	}
+	while (no_d > 1)
+	{
+		no_d--;
+		p *= 10;
+	}
+	while (p >= 1)
+	{
+		_putchar((n / p) % 10 + '0');
+		p /= 10;
+	}
 }
