ff17c89e2c0b9f1d0491d4b787ca67b2cd08a746
Badre ZIANI <badreziani@gmail.com>
2023-04-03 03:30:00 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
- *
- *
+ * _strchr - locates a character in a string
+ * @s: pointer to char
+ * @c: char
+ * Return: pointer
  */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+	
+	while (1)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
