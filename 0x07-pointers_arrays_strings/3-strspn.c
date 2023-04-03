d33292e1d48339f1816f37375771844523dd1e41
Badre ZIANI <badreziani@gmail.com>
2023-04-03 05:05:50 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
- *
- *
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to char
+ * @accept: pointer to char
+ * Return: unsigned int
  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0, i, j;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				break;
+			}
+		}
+	}
+	return (len);
+}
