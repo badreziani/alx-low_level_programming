f8da973c601d9abb7942ca01ca38f20176538d07
Badre ZIANI <badreziani@gmail.com>
2023-04-11 03:58:44 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
+	int i;
 
 	if (size == 0)
 		return (NULL);
@@ -19,6 +20,9 @@ char *create_array(unsigned int size, char c)
 	arr = (char *)malloc(size * sizeof(c));
 	if (arr == NULL)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
 
 	return (arr);
 }
