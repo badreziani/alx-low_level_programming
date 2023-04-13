318c57a87b4e2ea5624693deca09d990e46092e7
Badre ZIANI <badreziani@gmail.com>
2023-04-13 04:06:23 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,35 +12,29 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new_s;
-	unsigned int len, x, y;
+	unsigned int x, y;
 
-	if (s1 == 0)
-		s1 = "";
+	if (s1 == NULL)
+		*s1 = '\0';
 	if (s2 == 0)
-		s2 = "";
+		*s2 = '\0';
 
-	len = 0;
 	for (x = 0; s1[x] != '\0'; x++)
-		len++;
-	for (x = 0; s2[x] != '\0' || x < n; x++)
-		len++;
-	new_s = malloc(sizeof(*new_s) * (len + 1));
+		;
+	for (y = 0; s2[y] != '\0'; y++)
+		if (y == n)
+			break;
+	new_s = malloc(sizeof(*new_s) * (x + y + 1));
 	if (new_s == NULL)
 		return (NULL);
-	for (x = 0, y = 0; s1[x] != '\0' || (s2[y] != '\0' || y < n);)
+	for (x = 0; s1[x] != '\0'; x++)
+		new_s[x] = s1[x];
+	for (y = 0; s2[y] != '\0'; y++)
 	{
-		if (s1[x] == '\0')
-		{
-			new_s[x + y] = s2[y];
-			y++;
-		}
-		else
-		{
-			new_s[x + y] = s1[x];
-			x++;
-		}
+		if (y == n)
+			break;
+		new_s[x + y] = s2[y];
 	}
 	new_s[x + y] = '\0';
-
 	return (new_s);
 }
