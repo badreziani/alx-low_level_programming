7609dcda009f5fa93712b394932ff94d57c43416
Badre ZIANI <badreziani@gmail.com>
2023-04-13 05:59:56 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,35 +11,23 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *nstr, *empt;
-	unsigned int i, len, j;
-	unsigned int size;
+	char *new_s;
+	unsigned int x, y, len;
 
 	len = 0;
-	empt = "";
 	if (s1 == NULL)
-		s1 = empt;
+		*s1 = '\0';
 	if (s2 == NULL)
-		s2 = empt;
+		*s2 = '\0';
 	while (s1[len] != '\0')
 		len++;
-	size = (len + n) * sizeof(*nstr);
-	nstr = malloc(size + 1);
-	if (nstr == NULL)
+	new_s = malloc((len + n) * sizeof(*new_s) + 1);
+	if (new_s == 0)
 		return (NULL);
-	i = 0;
-	while (i < size && s1[i] != '\0')
-	{
-		nstr[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (i < size && s2[j] != '\0')
-	{
-		nstr[i] = s2[j];
-		i++;
-		j++;
-	}
-	nstr[i] = '\0';
-	return (nstr);
+	for (x = 0; s1[x] != '\0'; x++)
+		new_s[x] = s1[x];
+	for (y = 0; (x <= len + n) && s2[y] != '\0'; x++, y++)
+		new_s[x] = s2[y];
+	new_s[x] = '\0';
+	return (new_s);
 }
