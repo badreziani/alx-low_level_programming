efe1a50896eeae6cf8d3965eef92f2ba8894eaf2
Badre ZIANI <badreziani@gmail.com>
2023-04-19 04:23:31 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t a;
 
 	if (array != NULL && action != NULL)
+	{
 		for (a = 0; a < size; a++)
 			action(array[a]);
+	}
 }
