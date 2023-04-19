c0e81c48bdc6d5dfecff68431eef98566e0aa8f4
Badre ZIANI <badreziani@gmail.com>
2023-04-19 04:57:34 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,13 +6,11 @@
  * as a parameter on each element of an array
  * @array: pointer to the first element of the array
  * @size: size of the array
- * action: pointer to a function
+ * @action: pointer to a function
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int a;
-
 	if (array != NULL && action != NULL)
-		for (a = 0; a < size; a++)
-			(*action)(array[a]);
+		while ((array++) != NULL)
+			action(*array);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,1 +1,6 @@
 #include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer.
+ * @array: pointer to the 1st element of array
+ * @size: int 
