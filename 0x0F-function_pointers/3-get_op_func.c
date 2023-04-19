33a981dfcc1496414eb10cb4b6f57bc2d8cd3e86
Badre ZIANI <badreziani@gmail.com>
2023-04-19 15:58:45 -0700

    Task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * get_op_func - returns a pointer to the function
+ * that corresponds to the operator given as a parameter
+ * @s: pointer to char
+ * Return: always 0
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t operations[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+	};
+	int a;
+
+	if (s != NULL)
+	{
+		for (a = 0; a < 5; a++)
+		{
+			if (operations[a].op == s)
+				return (operstions[a].f);
+		}
+	}
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - entry point of the programm
+ * @argc: number of arguments
+ * @argv: array of values of arguments
+ * Return: nothing
+ */
+void main(int argc, char *argv[])
+{
+	int num1, num2, result, (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!(argv[2][0] == '+'
+			|| argv[2][0] == '-'
+			|| argv[2][0] == '*'
+			|| argv[2][0] == '/'
+			|| argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%')
+			&& atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+}
