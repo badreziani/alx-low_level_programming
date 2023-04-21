ad679ee07ee16dbd4d27f11334a2ab1ad88001cb
Badre ZIANI <badreziani@gmail.com>
2023-04-21 08:52:05 -0700

    Fix header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,8 +5,4 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list l);
-void print_integer(va_list l);
-void print_float(va_list l);
-void print_string(va_list l);
 #endif
