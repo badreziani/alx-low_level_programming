f9a36fcb5542cda13228b52692f6be24a11af78e
Badre ZIANI <badreziani@gmail.com>
2023-04-27 09:20:24 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * size_t print_list - prints all the elements of a list_t list
+ * print_list - prints all the elements of a list_t list
  * @h: list_t param
  * Return: the number of nodes
  */
@@ -15,7 +15,6 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		
 		h = h->next;
 		num++;
 	}
