c9296eafa606cbee6da5dc3674502db3dfc84e0b
Badre ZIANI <badreziani@gmail.com>
2023-04-27 15:23:50 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,9 @@ size_t print_list(const list_t *h)
 {
 	size_t num = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (h->str)
+		if (h->str != NULL)
 			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
