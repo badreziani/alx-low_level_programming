680ea6a34ebeeba5d3a915895fe51e7533d9cd05
Badre ZIANI <badreziani@gmail.com>
2023-04-27 09:03:37 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * size_t print_list
+ * @h: list_t param
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
