38704bfa73109ed87bd2730c030337c204733b3c
Badre ZIANI <badreziani@gmail.com>
2023-04-30 11:04:38 -0700

    Create task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,8 +12,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *prev;
 
 	if (*head == NULL || head == NULL)
-		return (-1);
+		return (-1);	
 	temp = *head;
+	if (index == 0)
+	{
+		*head = *head->next;
+		free(temp);
+		return (1);
+	}
 	while (temp != NULL && index > 0)
 	{
 		prev = temp;
