5ed496623066aa68da435a59eb03204bad5b4264
Badre ZIANI <badreziani@gmail.com>
2023-04-30 13:12:05 -0700

    Fix task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		node->next = temp;
 		*head = node;
-		return (node);
+		return (*head);
 	}
 	temp = *head;
 	while (temp != NULL && idx > 0)
