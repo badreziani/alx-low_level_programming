cd0b76dc605944f45cb3afd68877f180832b05c4
Badre ZIANI <badreziani@gmail.com>
2023-04-30 02:23:26 -0700

    Fix task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,11 +9,13 @@ size_t listint_len(const listint_t *h)
 {
 	size_t len = 0;
 
-	while (h != NULL)
+	if (h != NULL)
 	{
-		h = h->next;
-		len++;
+		while (h != NULL)
+		{
+			h = h->next;
+			len++;
+		}
 	}
-	
 	return (len);
 }
