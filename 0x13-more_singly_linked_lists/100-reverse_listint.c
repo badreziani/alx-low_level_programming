9063bcc1776b9d2ad2497ebaeed52592e1afb456
Badre ZIANI <badreziani@gmail.com>
2023-05-01 04:45:56 -0700

    Create task 100

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a listint_t linked list
+ * @head: pointer to linked list
+ * Returns: a pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *current, *next;
+
+	while (current != NULL)
+	{
+		next = 
+	}
+	return (*head);
+}
