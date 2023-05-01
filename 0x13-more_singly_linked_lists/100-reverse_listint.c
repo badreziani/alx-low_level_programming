121d365cfa44a0f97447182c2a394494abbd2e77
Badre ZIANI <badreziani@gmail.com>
2023-05-01 11:25:40 -0700

    Fix task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,8 +8,6 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *ptr1, *ptr2;
-	if (head == NULL || *head == NULL)
-		return (NULL);
 	if ((*head)->next == NULL)
 		return (*head);
 	ptr1 = NULL;
