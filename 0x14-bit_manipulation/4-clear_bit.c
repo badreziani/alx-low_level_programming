1b9f32c171cd8fcbbca9e6948606fa1f5736756a
Badre ZIANI <badreziani@gmail.com>
2023-05-04 15:10:01 -0700

    Update task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,9 +8,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index > 64)
 		return (-1);
 
-	*n = (1 << index) & *n;
+	*n = *n & ~(1 << index);
 	return (1);
 }
