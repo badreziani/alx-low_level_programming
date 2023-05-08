163fd1c018a7fc6ec25155cc2dbaf22d3ebb29db
Badre ZIANI <badreziani@gmail.com>
2023-05-08 16:40:50 -0700

    Update task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
                 text_content = "";
         for (len = 0; text_content[len] !=  '\0'; len++)
                 ;
-        fd = open(filename, O_WRONLY | O_APPEND)
+        fd = open(filename, O_WRONLY | O_APPEND);
         if (fd == -1)
                 return (-1);
 
