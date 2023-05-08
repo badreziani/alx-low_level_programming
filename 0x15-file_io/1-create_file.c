ad1e40ad768014f423e03484f575e81f015c3443
Badre ZIANI <badreziani@gmail.com>
2023-05-08 16:34:08 -0700

    Update task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,10 +17,9 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	for (len = 0; text_content[len] !=  '\0'; len++)
 		;
-
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	write(fd, text_content, len);
 	return (1);
