3538b5c9bb28423d51a0b09019b67b8ed0ad463f
Badre Ziani <badreziani@gmail.com>
2023-06-07 15:26:56 -0700

    Create task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * is_prime - detrmine whether x is prime or not
+ * @x: integer
+ * @y: integer
+ * Return: 1 or 0
+ */
+int is_prime(int x, int y)
+{
+	if (x <= 1)
+		return (0);
+	if (x % y == 0)
+		return (0);
+	if (x == y)
+		return (1);
+	return (is_prime(x, y + 1));
+}
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0
+ * @n: the number
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+int is_prime_number(int n)
+{
+	return (is_prime(n, 1));
+}
