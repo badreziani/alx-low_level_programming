a3ce2a0d75352154c13b4d11e8fa0c5acd987636
Badre Ziani <badreziani@gmail.com>
2023-06-07 15:58:58 -0700

    Create task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * is_p - returns 1 if a string is a palindrome and 0 if not
+ * @str: the string
+ * @len: the lenght of the string
+ * @idx: index
+ * Return: 0 or 1
+ */
+int is_p(char *str, int len, int idx)
+{
+	if (*str != *(str + idx))
+		return (0);
+	if (idx >= len)
+		return (1);
+	return (is_p(str + 1, len, idx - 1));
+}
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * @s: the string
+ * Return: 1 if a string is a palindrome and 0 if not
+ */
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = _strlen_recursion(s);
+	return (is_p(s, len / 2, len - 1));
+}
