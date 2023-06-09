7e220cc371117f1d7b0199a516d982f78e96b77f
Badre Ziani <badreziani@gmail.com>
2023-06-09 05:29:14 -0700

    Create task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,5 +9,13 @@
  */
 int main(int argc, char *argv[])
 {
+	if (argc > 2)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN
 #include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 #endif
