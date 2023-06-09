44a51e49d287e7cdc4697adcc9204db4d61a5920
Badre Ziani <badreziani@gmail.com>
2023-06-09 02:30:31 -0700

    Create task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - the entry point of the program
+ * Description: prints its name, followed by a new line.
+ * @argc: the number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
