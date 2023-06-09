3647e571c59d641bfd1492f0ab7ea7e938e9f923
Badre Ziani <badreziani@gmail.com>
2023-06-09 07:12:22 -0700

    Create task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,5 +9,20 @@
  */
 int main(int argc, char *argv[])
 {
+	int i, j, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%i\n", sum);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,6 @@
 #define MAIN
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 int _putchar(char c);
 #endif
