bca503614911e17211a2ae27756104623ca41954
Badre Ziani <badreziani@gmail.com>
2023-06-09 03:27:52 -0700

    Update task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,11 +7,9 @@
  * @argv: array of arguments
  * Return: 0
  */
-int main(int argc, char __attribute((unused)) *argv[])
+int main(int argc, char __attribute((unused)) * argv[])
 {
 	if (argc > 0)
 		printf("%d\n", argc - 1);
-	else
-		 printf("0\n");
 	return (0);
 }
