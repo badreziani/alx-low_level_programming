d578811e6de29259dadaf7577faba3a6559c9d4a
Badre Ziani <badreziani@gmail.com>
2023-06-12 07:00:22 -0700

    Create task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,3 +1,17 @@
 #include "main.h"
 /**
- *
+ * free_grid - frees a 2 dimensional grid
+ * @grid: the grid to be freed
+ * @height: the height of the grid
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	height--;
+	while (height >= 0)
+	{
+		free(grid[height]);
+		height--;
+	}
+	free(grid);
+}
