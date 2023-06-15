662162f049544b28a347be12711eef568a8f1de8
Badre Ziani <badreziani@gmail.com>
2023-06-15 06:59:13 -0700

    Update task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	else if (ptr == NULL)
 		ptr = malloc(new_size);
-	else if (new_size == 0 && !ptr)
-		return (NULL);
+	else if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		ptr = NULL;
+	}
 	return (ptr);
 }
