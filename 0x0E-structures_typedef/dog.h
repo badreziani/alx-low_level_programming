c1f3f2566aadf1c22b4d57c66ba2d54a810aa78a
Badre Ziani <badreziani@gmail.com>
2023-06-19 07:00:16 -0700

    Update task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
-
 /**
  * struct dog - a dog type
  * @name: the name of the dog
@@ -13,7 +11,7 @@ typedef struct dog
 	char *name;
 	int age;
 	char *owner;
-} dog;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
