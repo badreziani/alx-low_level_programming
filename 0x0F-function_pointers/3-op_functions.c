120cce0e0354fde853fa3c931ec80739a149e51c
Badre Ziani <badreziani@gmail.com>
2023-06-21 08:21:32 -0700

    Create task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum of a and b
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the diff of a and b
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of a and b
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the result of the div of a by b
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the sum of a and b
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
