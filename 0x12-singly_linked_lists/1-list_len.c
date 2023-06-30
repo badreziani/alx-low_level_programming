7b29c9bc9a6cf4b08cc90466683c1fb8d27a96e6
Badre Ziani <badreziani@gmail.com>
2023-06-30 04:10:36 -0700

    Create task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,10 +2,19 @@
 
 /**
  * list_len - returns the number of elements in a linked list_t list
- * h: the head of the linked list
+ * @h: the head of the linked list
  * Return: returns the number of elements in the list
  */
 size_t list_len(const list_t *h)
 {
+	size_t len = 0;
 
+	if (!h)
+		return (0);
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
 }
