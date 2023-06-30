d03a7f5750bcc8412c059b58560fb93b715f2bc4
Badre Ziani <badreziani@gmail.com>
2023-06-30 10:30:51 -0700

    Create task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,4 +1,4 @@
-#include <lists.h>
+#include "lists.h"
 
 /**
  * free_list -  frees a list_t list
@@ -7,5 +7,13 @@
  */
 void free_list(list_t *head)
 {
+	list_t *temp;
 
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
