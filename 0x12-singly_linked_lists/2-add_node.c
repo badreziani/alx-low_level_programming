568305eca40d7cca83554dbfb6c68fddc416d807
Badre Ziani <badreziani@gmail.com>
2023-06-30 10:13:30 -0700

    Create task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
-#include <lists.h>
+#include "lists.h"
+#include <string.h>
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
@@ -8,5 +9,15 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *ptr;
 
+	if (!(*head) || !head)
+		return (NULL);
+	ptr = malloc(sizeof(*head));
+	if (!ptr)
+		return (NULL);
+	ptr->str = strdup(str);
+	ptr->next = *head;
+	*head = ptr;
+	return (*head);
 }
