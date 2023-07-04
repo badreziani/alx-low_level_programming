14c66fcd33fa7011e5a02023ad1d072f1bb26137
Badre Ziani <badreziani@gmail.com>
2023-07-04 04:09:30 -0700

    Create task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number
+ * of elements in a linked listint_t list
+ * @h: the linked list
+ * Return: the number of elements in h
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len = 0;
+
+	if (h)
+	{
+		while (h)
+		{
+			h = h->next;
+			len++;
+		}
+	}
+	return (len);
+}
