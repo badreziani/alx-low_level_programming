6fd1d4a2d70446135d36fb849c84e1cb638d968b
Badre Ziani <badreziani@gmail.com>
2023-07-04 04:42:55 -0700

    Update task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * free_listint_safe - frees a listint_t list
  * @h: pointer to linked list
@@ -7,4 +6,25 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
+	listint_t *ptr;
+	size_t len;
+
+	len = 0;
+	if (!(*h) || !h)
+		return (0);
+	ptr = *h;
+	while (ptr)
+	{
+		if (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+			free(*h);
+			*h = ptr;
+			len++;
+		}
+	}
+	free(*h);
+	*h = NULL;
+	len++;
+	return (len);
 }
