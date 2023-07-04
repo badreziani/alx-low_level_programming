7e62946c3384ed414e65ad7444b668b3686300fe
Badre Ziani <badreziani@gmail.com>
2023-07-04 05:02:56 -0700

    Update task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,10 +10,10 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *ptr;
 	size_t len = 0;
 
-	if (!head)
+	if (head == NULL)
 		exit(98);
 	ptr = head;
-	while (ptr)
+	while (ptr != NULL)
 	{
 		printf("[%p] %d", (void *)ptr, ptr->n);
 		ptr = ptr->next;
