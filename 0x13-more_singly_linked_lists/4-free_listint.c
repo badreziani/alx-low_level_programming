ec8b9c8398ff90bfc4f11522dae8ea983f0f4f11
Badre Ziani <badreziani@gmail.com>
2023-07-04 04:13:00 -0700

    Create task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: list to free
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head)
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
+}
