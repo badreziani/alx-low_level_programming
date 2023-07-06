60551eac8fa4ca66f36625c73f9ec4a00436f254
Badre Ziani <badreziani@gmail.com>
2023-07-06 07:24:23 -0700

    Update task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,40 +1,29 @@
 #include "main.h"
 
 /**
- * _pow - calculates the power of 2
- * @b: the base, in our case it will be always 2
- * @p: the power or the exponent
- * Return: the result of the power
- */
-unsigned int _pow(unsigned int b, unsigned int p)
-{
-	if (p != 0)
-		return (b * _pow(b, p - 1));
-	return (1);
-}
-/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: the number to convert
  * Return: converted number or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int i, x;
+	unsigned int unit = 0, two = 1;
+	int len, i, bit;
 
-	if (!b || b[0] == '\0')
+	if (b == NULL)
 		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	for (i = 0; i < len; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[len - i - 1] < '0' || b[len - i - 1] > '1')
 			return (0);
+		if (b[len - i - 1] == '0')
+			bit = 0;
+		else
+			bit = 1;
+		unit += bit * two;
+		two *= 2;
 	}
-	x = --i;
-	while (x >= 0)
-	{
-		if (b[i - x] == '1')
-			num += _pow(2, i - x);
-		x--;
-	}
-	return (num);
+	return (unit);
 }
