fdec2e5377b68c1869c7587997bbefe9601532f9
Badre Ziani <badreziani@gmail.com>
2023-07-06 07:25:14 -0700

    Update task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int unit = 0, two = 1;
 	int len, i, bit;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 	for (len = 0; b[len] != '\0'; len++)
 		;
