ae424e3f94cfb119f370d21a601ff74e0363357b
Badre Ziani <badreziani@gmail.com>
2023-07-10 05:52:31 -0700

    Update task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,7 +8,7 @@
  * in the ELF header at the start of an ELF file
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char __attribute__((__unused__)) *argv[])
 {
 	return (0);
 }
