f1ea44a877925ab369c4405b235beee22f157bd5
Badre Ziani <badreziani@gmail.com>
2023-07-10 05:50:54 -0700

    Create task 4

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point of the program
+ * @argc: count of args
+ * @argv: array og args
+ * Description: displays the information contained
+ * in the ELF header at the start of an ELF file
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
