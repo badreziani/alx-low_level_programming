6821e6c2625cf652119de536be67c639ec7b0633
Badre Ziani <badreziani@gmail.com>
2023-07-10 04:04:07 -0700

    Create task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,17 +1,5 @@
 #include "main.h"
-/**
- * _strlen - counts the length of a string
- * @s: the string
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int len = 0;
 
-	while (s[len] != '\0')
-		len++;
-	return (len);
-}
 /**
  * create_file - creates a file
  * @filename: the name of the file to create
