4f0c2ba7825f985d862516080d18cbc2b3dc36d8
Badre Ziani <badreziani@gmail.com>
2023-11-27 05:46:54 -0800

    Fisrt commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: the filename
+ * @letters: the letters to be printed to the file
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, size;
+	char *buf;
+
+	buf = malloc(letters);
+	if  (!buf || !filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	size = read(fd, buf, letters);
+	size = write(STDOUT_FILENO, buf, size);
+	close(fd);
+	return (size);
+}
