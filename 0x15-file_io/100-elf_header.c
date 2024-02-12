29593f499f3448f0e9939d80c24027009441a674
Badre Ziani <badreziani@gmail.com>
2024-02-12 01:04:13 -0800

    Task 4

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,18 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ * @argc: arguments count
+ * @argv: arguments values
+ *
+ * Description: displays the information contained
+ * in the ELF header at the start of an ELF file
+ * Return: Always 0
+ */
+int main(int argc, const char *argv[])
+{
+	return (0);
+}
