8de052023d1c3bf1eadd81710897e07e2798070b
Badre Ziani <badreziani@gmail.com>
2024-04-12 15:59:22 +0000

    Task 4 update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 	unsigned long int idx;
 
-	if (!ht || !key)
+	if (!ht || !ht->array || ht->size == 0 ||
+			!key || *key == '\0')
 		return (NULL);
 	idx = key_index((unsigned char *)key, ht->size);
 	if (!ht->array[idx])
